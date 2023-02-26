b4783dfa6629c828d58ed6e8f0356f6206da351e
ramjdev <126394117+ramjdev@users.noreply.github.com>
2023-02-26 17:56:56 +0530

    Add files via upload

---
+++ ArthematicOperationsTest1.java
@@ -0,0 +1,29 @@
+/**
+* +
+Addition 
+(can be used for String concatenation also)
+-
+Subtraction 
+*
+Multiplication 
+/
+Division 
+%
+Modulus/ Remainder 
+*/
+public class ArthematicOperationsTest1 
+{
+	public static void main(String[] args){
+		
+		int a= 5;
+		int b= 6;
+		
+		float f1 = 2.2f;
+
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
